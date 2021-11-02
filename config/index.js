8ca5e30663801ba4146b8c62ddfa5bbdefea7499
qasimraheem <qasimmehmood13936@gmail.com>
2021-11-02 09:09:20 +0500

    worked on removing empty blocks (changing .env)

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ export const {
     TEST_NET_WS = `${NET_WS}${TEST}:${TEST_NET_PORT_WS}`,
     // MAIN_NET_WS = `${NET_WS}${MAIN}:${MAIN_NET_PORT_WS}`,
     // MAIN_NET_HTTP= `${NET_HTTP}${MAIN}:${MAIN_NET_PORT_HTTP}`
-    MAIN_NET_WS = 'wss://node.watchfornax.com/ws',
-    MAIN_NET_HTTP= 'https://node.watchfornax.com/rpc',
+    MAIN_NET_WS,
+    MAIN_NET_HTTP,
     MONGO_URI,
 } = parsed;
